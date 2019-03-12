f9c0bc251bbc9da23da177000849ec888e3c8812
mark <aj.zd3z@gmail.com>
2019-03-11 20:44:47 -0400

    Posterior calculation seems to not work, going to break things

--- app.R
+++ app.R
@@ -1,26 +1,57 @@
 library(shiny)
 library(shinyjs)
 
-# strtoi(strsplit("2,3,4,5", split = ",")[[1]])
-
+# reference variables for output
 basePriorDistribution <- c("Standard uniform", "Standard normal", "Randomly generated")
 parameterType <- c("bernoulliParameterValue", "poissonParameterValue", "geometricParameterValue")
 correspondingInputBoxes <- list("bernoulliParameterValue" = c("_numberHeads", "_totalFlips"), "poissonParameterValue" = "_csv", "geometricParameterValue" = "_csv")
 
-xSubdivisions <- c()
+# Global Variables 
+parameterSubdivisions <- c()
 subdivisionProbability <- c()
+likelihoodValues <- c()
 initialSubdivisions <- 100
 likelihoodFormula <- 1 # bernoulli 
+firstRun <- TRUE
+posteriorProbability <- c()
+
+initialHeads <- 10
+initialTotalFlips <- 40
+exampleOutcomeString <- "3, 3, 4, 5, 6, 3, 2, 2, 1, 3, 7, 8, 6, 4, 4"
+exampleProbabilityString <- "0.65, 0.7, 0.9, 0.3, 0.2"
 
 # N = total flips, z = heads
 bernoulliLikelihood <- function(theta, N, z) {
-  return (theta^z * theta^(N - z))
+  return (theta^z * (1 - theta)^(N - z))
 }
 
-# lambda is poisson parameter, outcome is a string like "1,2,3,4"
-poissonLikelihood <- function(lambda, outcomeString) {
+# lambda is vector of probabilities (in this case, parameterSubdivisions), outcome is a string like "1,2,3,4"
+poissonLikelihood <- function(lambdas, outcomeString) {
   outcomes <- strtoi(strsplit(outcomeString, split = ",")[[1]])
-  return ((prod(exp(-1 * lambda * outcomes)) * product(lambda^outcomes))/(product(factorial(outcomes))))
+  if (outcomes == 0 || length(outcomes) == 0) {
+    stop()
+  }
+  
+  # Stop gap solution
+  if (length(outcomes) < length(lambdas)) {
+    outcomes <- rep_len(outcomes, length.out = length(lambdas))
+  }
+  
+  likelihood <- vector(length = length(lambdas))
+  n <- length(outcomeString)
+  
+  # for each likelihood entry is a product of 
+  for (lambda in 1:length(lambdas)) {
+    # e^-n*lambda
+    poisExp = exp(-1 * n * lambda)
+    # lambda^sum(outcomes)
+    lambdaPower = lambda^sum(outcomes)
+    denominator = prod(factorial(outcomes))
+
+    likelihood[x] = poisExp * lambdaPower / denominator
+  }
+  return(likelihood)
+  # return ((prod(exp(-1 * lambda * outcomes)) * prod(lambda^outcomes))/(prod(factorial(outcomes))))
 }
 
 # lambda is poisson parameter, outcome is a string like "1,2,3,4"
@@ -28,22 +59,30 @@ geometricLikelihood <- function(theta, probabilityString) {
   
 }
 
+computePosterior <- function(likelihoodValues, priorValues) {
+  print(likelihoodValues * priorValues)
+  print(sum(likelihoodValues * priorValues))
+  return((likelihoodValues * priorValues)/sum(likelihoodValues * priorValues))
+}
+
 setInitialPriorDistribution <- function(likelihoodFormula, subdivisions, densityFunction) {
   from <- 0
   to <- 0
   if (likelihoodFormula == 2) { # poisson
-    from <- 0
+    from <- 1
     to <- 100
   } else { # bernoulli or geometric
     from <- 0
     to <- 1
   }
-  xSubdivisions <<- seq(from, to, length.out = subdivisions)
-  subdivisionProbability <<- densityFunction(xSubdivisions)
+  parameterSubdivisions <<- seq(from, to, length.out = subdivisions)
+  subdivisionProbability <<- densityFunction(parameterSubdivisions)
   subdivisionProbability <<- subdivisionProbability/sum(subdivisionProbability)
 }
 
+# initialize the default information 
 setInitialPriorDistribution(1, initialSubdivisions, dunif)
+likelihoodValues <- bernoulliLikelihood(parameterSubdivisions, initialTotalFlips, initialHeads)
 
 ui <- fluidPage(
   useShinyjs(),
@@ -76,15 +115,22 @@ ui <- fluidPage(
                               ),
                               selected = 1
                               ),
-                 numericInput(inputId = parameterType[1], label = "Bernoulli Parameter Value", value = 0.5),
-                 numericInput(inputId = paste(parameterType[1], "_numberHeads", sep = ''), label = "Number of heads", value = 0.5),
-                 numericInput(inputId = paste(parameterType[1], "_totalFlips", sep = ''), label = "Total flips", value = 0.5),
-                 numericInput(inputId = parameterType[2], label = "Poisson Parameter Value", value = 3),
-                 numericInput(inputId = paste(parameterType[2], "_csv", sep = ''), label = "Comma seperated outcomes", "2,3,4,2"),
-                 numericInput(inputId = parameterType[3], label = "Geometric Parameter Value", value = 3),
-                 numericInput(inputId = paste(parameterType[3], "_csv", sep = ''), label = "Comma seperated probabilities", "2,3,4,2"),
+                 numericInput(inputId = parameterType[1], label = "True Bernoulli Parameter Value", value = 0.5),
+                 numericInput(inputId = paste(parameterType[1], "_numberHeads", sep = ''), label = "Number of heads", value = initialHeads),
+                 numericInput(inputId = paste(parameterType[1], "_totalFlips", sep = ''), label = "Total flips", value = initialTotalFlips),
                  
-                 actionButton("update", "Compute posterior")
+                 numericInput(inputId = parameterType[2], label = "True Poisson Parameter Value", value = 3),
+                 textInput(   inputId = paste(parameterType[2], "_csv", sep = ''), label = "Comma seperated outcomes", exampleOutcomeString),
+                 
+                 numericInput(inputId = parameterType[3], label = "True Geometric Parameter Value", value = 3),
+                 textInput(   inputId = paste(parameterType[3], "_csv", sep = ''), label = "Comma seperated probabilities", exampleProbabilityString),
+                 actionButton("setLikelihood", "Set Likelihood distribution"),
+                 br(),
+                 br(),
+                 br(),
+                 helpText("Use grid method to compute posterior"),
+                 actionButton("updatePosterior", "Compute posterior"),
+                 actionButton("posteriorToPrior", "Copy posterior to prior")
     ),
     mainPanel(
       plotOutput(outputId = "priorPlot"), 
@@ -94,25 +140,30 @@ ui <- fluidPage(
   )
 )
 
-firstRunPrior <- TRUE
-firstRunLikelihood <- TRUE
 server <- function(input, output, session) {
   # Shows the specific input box for the likelihood chosen
   # Using shinyjs
   observe({
-    if (firstRunPrior) {
-      print("first run")
+    if (firstRun) {
+      #print("first run")
       output$priorPlot <- renderPlot({
-        plot(xSubdivisions, subdivisionProbability)
+        plot(parameterSubdivisions, subdivisionProbability, type = "h", lty = 1, lwd = 5, pch = 20, cex = 0.5, col = "blue")
       })
-      firstRunPrior <<- FALSE
+      
+      output$likelihoodPlot <- renderPlot({
+        plot(parameterSubdivisions, bernoulliLikelihood(parameterSubdivisions, initialTotalFlips, initialHeads), lty = 1, lwd = 2, pch = 20, cex = 0.5)
+      })
+      
+      firstRun <<- FALSE
     }
     currentParameter = parameterType[strtoi(input$likelihoodFormulas)]
     show(currentParameter)
     
     # shows the boxes needed through searching the list object correspondingInputBoxes
     for (parameterToShow in correspondingInputBoxes[currentParameter]) {
-      show(paste(currentParameter, parameterToShow, sep = ''))
+      for (boxesToShow in parameterToShow) {
+          show(paste(currentParameter, boxesToShow, sep = ''))
+      }
     }
     
     # hides the unneeded boxes using the list object correspondingInputBoxes
@@ -127,41 +178,85 @@ server <- function(input, output, session) {
     }
   })
   
-  #updatePrior <- eventReactive(input$setPrior, {
+  # Updates plot upon button click (couldn't get it working using eventReactive)
   updatePrior <- observeEvent(input$setPrior, {
     selectedLikelihood <- strtoi(input$likelihoodFormulas)
     selectedSubdivisions <- input$subdivisions
+    
+    if (length(parameterSubdivisions) == 0 ||
+        selectedLikelihood < 1 || selectedLikelihood > 3 ||
+        selectedSubdivisions <= 0) {
+      stop()
+    }
+        
     if (input$priorFormulas == "1") {
       setInitialPriorDistribution(selectedLikelihood, selectedSubdivisions, dunif)
     } else if (input$priorFormulas == "2") {
       setInitialPriorDistribution(selectedLikelihood, selectedSubdivisions, dnorm)
     } else if (input$priorFormulas == "3") {
       setInitialPriorDistribution(selectedLikelihood, selectedSubdivisions, runif)
+    } else {
+      stop()
     }
     
     output$priorPlot <- renderPlot({
-      plot(xSubdivisions, subdivisionProbability)
+      plot(parameterSubdivisions, subdivisionProbability, pch = 20, lty = 2)
     })
   })
   
-  updateLikelihood <- eventReactive(input$likelihoodFormulas, {
-  })
-  
-  output$likelihoodPlot <- renderPlot({
-    if (firstRunLikelihood) {
-      firstRunLikelihood <<- FALSE
-    } else {
-      updateLikelihood()
-    }
-  })
-  
-  output$posteriorPlot <- renderPlot({
-    x    <- faithful$waiting
-    bins <- seq(min(x), max(x), length.out = 100)
+  updateLikelihood <- observeEvent(input$setLikelihood, {
     
-    hist(x, breaks = bins, col = "#75AADB", border = "white",
-         xlab = "Waiting time to next eruption (in mins)",
-         main = "Histogram of waiting times")
+    baseFormula <- input$likelihoodFormulas
+    selectedLikelihood <- strtoi(input$likelihoodFormulas)
+    selectedSubdivisions <- input$subdivisions
+    
+    # in case the parameter support set changes
+    if (input$priorFormulas == "1") {
+      setInitialPriorDistribution(selectedLikelihood, selectedSubdivisions, dunif)
+    } else if (input$priorFormulas == "2") {
+      setInitialPriorDistribution(selectedLikelihood, selectedSubdivisions, dnorm)
+    } else if (input$priorFormulas == "3") {
+      setInitialPriorDistribution(selectedLikelihood, selectedSubdivisions, runif)
+    } else {
+      stop()
+    }
+        
+    if (length(parameterSubdivisions) == 0 ||
+        selectedLikelihood < 1 || selectedLikelihood > 3) {
+      stop()
+    }    
+    if (baseFormula == "1") { # bernoulliLikelihood
+      z <- input$bernoulliParameterValue_numberHeads
+      N <- input$bernoulliParameterValue_totalFlips
+      likelihoodValues <<- bernoulliLikelihood(parameterSubdivisions, N, z)
+    } else if (baseFormula == "2") { # poissonLikelihood
+      outcomeString <- input$poissonParameterValue_csv
+      likelihoodValues <<- poissonLikelihood(parameterSubdivisions, outcomeString)
+      print(poissonLikelihood(parameterSubdivisions, outcomeString))
+    } else if (baseFormula == "3") { # geometricLikelihood
+      stop()
+    } else {
+      stop()
+    }
+    
+    output$likelihoodPlot <- renderPlot({
+      plot(parameterSubdivisions, likelihoodValues, lty = 1, pch = 20)
+    })
+  })
+  
+  updatePosteriorGraph <- observeEvent(input$updatePosterior, {
+    posteriorProbability <<- computePosterior(likelihoodValues, subdivisionProbability)
+    output$posteriorPlot <- renderPlot({
+      plot(parameterSubdivisions, posteriorProbability, lty = 1, pch = 20)
+    })
+  })
+  
+  copyPosteriortoPrior <- observeEvent(input$posteriorToPrior, {
+    print(subdivisionProbability == posteriorProbability)
+    subdivisionProbability <<- posteriorProbability
+    output$priorPlot <- renderPlot({
+      plot(parameterSubdivisions, subdivisionProbability, lty = 1, pch = 20)
+    })
   })
 }
 
